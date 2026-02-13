8e2f80d5bfc6448a61dc8ac73f2c24a1423857a6
Malimbita3994 <japhetfaustine39@gmail.com>
2026-02-13 14:52:34 +0300

    Mkoa entity was Added....!

--- services/usecase/mkoa/_mkoa_service.go
+++ services/usecase/mkoa/_mkoa_service.go
@@ -31,6 +31,123 @@ func (s *service) Create(ctx context.Context, name, code string, createdBy int64
 
 	return m, nil
 }
+package mkoa
+
+import (
+	"library/package/log"
+	"library/services/entity"
+	"library/services/repository"
+)
+
+type Service struct {
+	repo Repository
+}
+
+func NewService() UseCase {
+	repo := repository.NewMkoa()
+	return &Service{
+		repo: repo,
+	}
+}
+
+/*
+   CREATE
+*/
+func (s *Service) Create(name string, code string, createdBy int32) (int32, error) {
+
+	mkoa, err := entity.NewMkoa(name, code, int64(createdBy))
+	if err != nil {
+		return int32(mkoa.ID), err
+	}
+
+	mkoaID, err := s.repo.Create(mkoa)
+	if err != nil {
+		return int32(mkoa.ID), err
+	}
+
+	return mkoaID, err
+}
+
+/*
+   LIST
+*/
+func (s *Service) List(filter *entity.MkoaFilter) ([]*entity.Mkoa, int32, error) {
+
+	mkoaData, totalCount, err := s.repo.List(filter)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return mkoaData, totalCount, nil
+}
+
+/*
+   GET
+*/
+func (s *Service) Get(id int32) (*entity.Mkoa, error) {
+
+	mkoaData, err := s.repo.Get(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return mkoaData, nil
+}
+
+/*
+   UPDATE
+*/
+func (s *Service) Update(e *entity.Mkoa) (int32, error) {
+
+	err := e.ValidateUpdate()
+	if err != nil {
+		log.Error(err)
+		return int32(e.ID), err
+	}
+
+	err = s.repo.Update(e)
+	if err != nil {
+		return int32(e.ID), err
+	}
+
+	return int32(e.ID), nil
+}
+
+/*
+   SOFT DELETE
+*/
+func (s *Service) SoftDelete(id, deletedBy int32) error {
+
+	_, err := s.Get(id)
+	if err != nil {
+		return err
+	}
+
+	err = s.repo.SoftDelete(id, deletedBy)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+/*
+   HARD DELETE
+*/
+func (s *Service) HardDelete(id int32) error {
+
+	_, err := s.Get(id)
+	if err != nil {
+		return err
+	}
+
+	err = s.repo.HardDelete(id)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
 
 /*
 GET ALL
--- services/usecase/mkoa/mkoa_interface.go
+++ services/usecase/mkoa/mkoa_interface.go
@@ -1,22 +1,31 @@
 package mkoa
 
 import (
-	"context"
 	"library/services/entity"
 )
 
-type Repository interface {
-	Insert(ctx context.Context, m *entity.Mkoa) error
-	GetAll(ctx context.Context) ([]*entity.Mkoa, error)
-	GetByID(ctx context.Context, id int64) (*entity.Mkoa, error)
-	Update(ctx context.Context, m *entity.Mkoa) error
-	SoftDelete(ctx context.Context, m *entity.Mkoa) error
+type Reader interface {
+	Get(id int32) (*entity.Mkoa, error)
+	List(filter *entity.MkoaFilter) ([]*entity.Mkoa, int32, error)
 }
 
-type Usecase interface {
-	Create(ctx context.Context, name, code string, createdBy int64) (*entity.Mkoa, error)
-	GetAll(ctx context.Context) ([]*entity.Mkoa, error)
-	GetByID(ctx context.Context, id int64) (*entity.Mkoa, error)
-	Update(ctx context.Context, m *entity.Mkoa) error
-	Delete(ctx context.Context, id int64, deletedBy int64) error
+type Writer interface {
+	Create(e *entity.Mkoa) (int32, error)
+	Update(e *entity.Mkoa) error
+	SoftDelete(id, deletedBy int32) error
+	HardDelete(id int32) error
+}
+
+type Repository interface {
+	Reader
+	Writer
+}
+
+type UseCase interface {
+	Create(name string, code string, createdBy int32) (int32, error)
+	List(filter *entity.MkoaFilter) ([]*entity.Mkoa, int32, error)
+	Get(id int32) (*entity.Mkoa, error)
+	Update(e *entity.Mkoa) (int32, error)
+	SoftDelete(id, deletedBy int32) error
+	HardDelete(id int32) error
 }
